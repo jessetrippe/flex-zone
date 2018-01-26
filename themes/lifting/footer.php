d81978ebae885a07635fac556c5e3f5948ff4ffc
Jesse Trippe <jesse@jessetrippe.com>
2018-01-25 19:54:14 -0800

    Removing site-container div

--- themes/lifting/footer.php
+++ themes/lifting/footer.php
@@ -7,12 +7,11 @@
 
 ?>
 
-	</main>
+</main>
 
-	<footer class="site-footer text-center pt-4 pb-3 px-2">
-		<p class="m-0 small text-muted">Copyright &copy; <?php echo date("Y"); ?> Jesse Trippe. All rights reserved.</p>
-	</footer>
-</div><!-- .site-content-container -->
+<footer class="site-footer text-center pt-4 pb-3 px-2">
+	<p class="m-0 small text-muted">Copyright &copy; <?php echo date("Y"); ?> Jesse Trippe. All rights reserved.</p>
+</footer>
 
 <button type="button" id="overlay-backdrop" class="overlay" style="display:none;"></button>
 
--- themes/lifting/header.php
+++ themes/lifting/header.php
@@ -21,25 +21,23 @@
 
 <body <?php body_class(); ?>>
 
-	<header id="masthead" class="site-header border-bottom d-flex bg-white">
-		<div class="w-33 p-2 h6 m-0">
-			<nav id="site-navigation" class="main-navigation">
-				<?php if ( !is_home() ) :
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id' => 'primary-menu',
-					) );
-				else : ?>
-					<button type="button" id="user-toggle" class="d-block" data-toggle="modal" data-target="#sign-out-screen">
-						<svg class="d-block" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 22 22">
-						  <circle class="fill-silver" cx="11" cy="11" r="11"/>
-						  <path class="fill-black" fill-rule="nonzero" d="M12,13.041 L12,12.216 C13.102,11.595 14,10.048 14,8.5 C14,6.015 14,4 11,4 C8,4 8,6.015 8,8.5 C8,10.048 8.898,11.595 10,12.216 L10,13.041 C6.608,13.318 4,14.985 4,17 L18,17 C18,14.985 15.392,13.318 12,13.041 Z"/>
-						</svg>
-					</button>
-				<?php endif; ?>
-			</nav>
-		</div>
-		<div class="w-33 text-center p-2 h6 m-0 text-uppercase">
+	<header id="masthead" class="site-header border-bottom bg-white">
+		<nav id="site-navigation" class="main-navigation p-2 float-left">
+			<?php if ( !is_home() ) :
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id' => 'primary-menu',
+				) );
+			else : ?>
+				<button type="button" id="user-toggle" class="d-block" data-toggle="modal" data-target="#sign-out-screen">
+					<svg class="d-block" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 22 22">
+					  <circle class="fill-silver" cx="11" cy="11" r="11"/>
+					  <path class="fill-black" fill-rule="nonzero" d="M12,13.041 L12,12.216 C13.102,11.595 14,10.048 14,8.5 C14,6.015 14,4 11,4 C8,4 8,6.015 8,8.5 C8,10.048 8.898,11.595 10,12.216 L10,13.041 C6.608,13.318 4,14.985 4,17 L18,17 C18,14.985 15.392,13.318 12,13.041 Z"/>
+					</svg>
+				</button>
+			<?php endif; ?>
+		</nav>
+		<div class="py-2 h6 m-0 text-uppercase text-center">
 			<?php
 				if( is_home() ) {
 					bloginfo('name');
@@ -49,8 +47,6 @@
 				}
 			?>
 		</div>
-		<div class="w-33 text-right h6 m-0 p-2">&nbsp;</div>
 	</header><!-- #masthead -->
 
-	<div class="site-content-container">
-		<main id="content" class="site-content">
+	<main id="content" class="site-main">
